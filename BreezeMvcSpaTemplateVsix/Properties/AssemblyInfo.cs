bb7f10377ecc7df9ee7e26fb4c2fb8799849f634
Ward Bell <wardbell@hotmail.com>
2013-02-18 17:30:44 -0800

    Fixed to run in .NET 4.0 as well; renamed to breezetemplate.vsix The rename was at the request of MS

--- BreezeMvcSpaTemplateVsix/Properties/AssemblyInfo.cs
+++ BreezeMvcSpaTemplateVsix/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BreezeMvcSpa")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("breezetemplate")]
+[assembly: AssemblyDescription("Breeze Mvc SPA Template")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BreezeMvcSpa")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("breezetemplate")]
+[assembly: AssemblyCopyright("IdeaBlade @ 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
