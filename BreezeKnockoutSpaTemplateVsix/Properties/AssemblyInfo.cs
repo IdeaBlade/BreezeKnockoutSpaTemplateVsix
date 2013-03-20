2a7c1f081f1bf516ca53eea20cbaebb119e00357
Ward Bell <wardbell@hotmail.com>
2013-03-19 18:38:08 -0700

    Upgraded to Breeze v.1.2.5 and moved contents around to more expected folders

--- BreezeKnockoutSpaTemplateVsix/Properties/AssemblyInfo.cs
+++ BreezeKnockoutSpaTemplateVsix/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("breezetemplate")]
-[assembly: AssemblyDescription("Breeze Mvc SPA Template")]
+[assembly: AssemblyTitle("breezeknockouttemplate")]
+[assembly: AssemblyDescription("Breeze/Knockout Mvc SPA Template")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("breezetemplate")]
+[assembly: AssemblyProduct("breezeknockouttemplate")]
 [assembly: AssemblyCopyright("IdeaBlade @ 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
